0cb18ab0f2e6c51d17ce156e985909e39bafb016
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-09-21 16:59:06 -0300

    App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
+  const { getByText } = render(<Router><App /></Router>);
+  const linkElement = getByText(/senha/i);
   expect(linkElement).toBeInTheDocument();
 });
