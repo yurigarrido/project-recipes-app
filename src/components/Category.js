27d0bf1afdc5b41ac4c5f31179928e71b1e9cf26
Gabriel <lenzordmusic@gmail.com>
2021-09-24 18:20:45 -0400

    req 28 ao 32 menos o 30 done

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,9 +2,26 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import useFetch from '../hooks/useFetch';
 
+const ifHandle = (request, categoryName, pageName, clicked) => {
+  if (pageName === 'comidas') {
+    if (categoryName === 'All' || clicked === false) {
+      request('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    } else {
+      request(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`);
+    }
+  } else if (pageName === 'bebidas') {
+    if (categoryName === 'All' || clicked === false) {
+      request('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    } else {
+      request(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoryName}`);
+    }
+  }
+};
+
 const Category = ({ pageName }) => {
   const [categoryList, setCategoryList] = useState(null);
   const { request, categories } = useFetch();
+  const [clicked, setClicked] = useState(false);
   const [aux, setAux] = useState(false);
 
   if (aux === false) {
@@ -29,15 +46,23 @@ const Category = ({ pageName }) => {
   }, [categories, pageName]);
 
   const handleClick = (categoryName) => {
-    if (pageName === 'comidas') {
-      request(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`);
-    } else if (pageName === 'bebidas') {
-      request(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoryName}`);
-    }
+    setClicked(!clicked);
+    ifHandle(request, categoryName, pageName, !clicked);
   };
 
   return (
     <section>
+      {
+        categoryList !== null && (
+          <button
+            data-testid="All-category-filter"
+            type="button"
+            onClick={ () => handleClick('All') }
+          >
+            All
+          </button>
+        )
+      }
       {
         categoryList !== null && categoryList.map((category, index) => (
           <button
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './css/recipeCard.module.css';
 
@@ -6,20 +7,26 @@ const RecipeCard = ({ products, pageName }) => (
   <section>
     {
       products.map((product, index) => (
-        <div
-          data-testid={ `${index}-recipe-card` }
+        <Link
+          to={ `/${pageName}/${pageName === 'bebidas' ? product.idDrink
+            : product.idMeal}` }
           key={ index }
-          className={ styles.card }
         >
-          <img
-            data-testid={ `${index}-card-img` }
-            src={ pageName === 'comidas' ? product.strMealThumb : product.strDrinkThumb }
-            alt={ pageName === 'comidas' ? product.strMeal : product.strDrink }
-          />
-          <span data-testid={ `${index}-card-name` }>
-            { pageName === 'comidas' ? product.strMeal : product.strDrink }
-          </span>
-        </div>
+          <div
+            data-testid={ `${index}-recipe-card` }
+            className={ styles.card }
+          >
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ pageName === 'comidas' ? product.strMealThumb
+                : product.strDrinkThumb }
+              alt={ pageName === 'comidas' ? product.strMeal : product.strDrink }
+            />
+            <span data-testid={ `${index}-card-name` }>
+              { pageName === 'comidas' ? product.strMeal : product.strDrink }
+            </span>
+          </div>
+        </Link>
       ))
     }
   </section>
