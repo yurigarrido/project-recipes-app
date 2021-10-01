09c42d6db7a9059eb7fe8f3fbe5be338047b8163
Erick Santos <erickosantos.dev@gmail.com>
2021-10-01 19:42:38 -0300

    req. 75-77 - Done (please 80%)

--- src/components/exploreComponents/RecipeCardExplore.js
+++ src/components/exploreComponents/RecipeCardExplore.js
@@ -1,34 +1,72 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import useFetchRandom from '../../hooks/useFetchRandom';
+import useFetch from '../../hooks/useFetch';
 
-// const recipeCardExplore = ({list}) => {
-//   const pageName = window.location.pathname.split('/')[1];
+const RecipeCardExplore = () => {
+  const category = window.location.pathname.split('/')[2];
+  const categoryName = category === 'comidas' ? 'meals' : 'drinks';
+  const db = category === 'bebidas' ? 'thecocktaildb' : 'themealdb';
+  const [list, setList] = useState(null);
+  const { request: requestIng, data: dataIng } = useFetchRandom();
+  const { request, data } = useFetch();
+  const history = useHistory();
 
-//   return (
-//     <div>
-//       {list.map((el, index) => {
-//         return (
-//           <div>
-//           {/* ta faltando implementar essa parte  */}
-//           <span
-//           data-testid={`${index}-ingredient-card`}
-//           >
-//             ingredientes
-//           </span>
-//           <img
-//             data-testid={`${index}-ingredient-card`}
-//             src={ pageName === 'bebidas' ? el.strDrinkThumb : el.strMealThumb }
-//             alt={ pageName === 'bebidas' ? el.strDrink : el.strMeal}
-//           />
-//           <span
-//             data-testid={`${index}-card-name`}
-//           >
-//             { pageName === 'bebidas' ? el.strDrink : el.strMeal}
-//           </span>
-//         </div>
-//       })
-//       }
-//     </div>
-//   );
-// };
+  useEffect(() => {
+    requestIng(`https://www.${db}.com/api/json/v1/1/list.php?i=list`);
+  }, [category]);
 
-// export default recipeCardExplore;
+  useEffect(() => {
+    const twelve = 12;
+    if (dataIng) {
+      if (category === 'comidas') {
+        setList(
+          dataIng[categoryName].slice(0, twelve).map((ing) => ing.strIngredient),
+        );
+      } else {
+        setList(
+          dataIng[categoryName].slice(0, twelve).map((ing) => ing.strIngredient1),
+        );
+      }
+    }
+  }, [dataIng, categoryName, category]);
+
+  useEffect(() => {
+    if (data) {
+      history.push(`/${category}`);
+    }
+  }, [data, history, category]);
+
+  const handleOnClick = (el) => {
+    el.replace(' ', '_');
+    request(`https://www.${db}.com/api/json/v1/1/filter.php?i=${el}`);
+  };
+
+  return (
+    <div>
+      {
+        list && list.map((el, index) => (
+          <button
+            type="button"
+            onClick={ () => handleOnClick(el) }
+            key={ index }
+            data-testid={ `${index}-ingredient-card` }
+          >
+            <span
+              data-testid={ `${index}-card-name` }
+            >
+              {el}
+            </span>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ `https://www.${db}.com/images/ingredients/${el}-Small.png` }
+              alt={ el }
+            />
+          </button>
+        ))
+      }
+    </div>
+  );
+};
+
+export default RecipeCardExplore;
--- src/pages/explore/ExploreDrinkIng.jsx
+++ src/pages/explore/ExploreDrinkIng.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Header, Footer } from '../../components';
+import RecipeCardExplore from '../../components/exploreComponents/RecipeCardExplore';
 
 const ExploreDrinkIng = () => (
   <div>
     <Header title="Explorar Ingredientes" search={ false } />
+    <RecipeCardExplore />
     <Footer />
   </div>
 );
