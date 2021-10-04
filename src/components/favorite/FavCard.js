7709474617705779cab9e6b8f29b2adbc5081046
yurigarrido <y.garrido.yuri@gmail.com>
2021-10-04 19:21:15 -0300

    req. 65 - done

--- src/components/favorite/FavCard.js
+++ src/components/favorite/FavCard.js
@@ -1,56 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './css/favCard.module.css';
 import ShareButton from './ShareButton';
 import FavoreButton from './FavButton';
 
-const FavCard = () => {
-  const [recipes, setRecipes] = useState(null);
-  const [local, setLocal] = useState(null);
-  const [aux, setAux] = useState(false);
+const FavCard = ({ recipes, aux, setAux }) => (
+  <div>
+    {recipes && recipes.map((recipe, index) => (
+      <div
+        className={ styles.card }
+        key={ index }
+      >
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <img
+            data-testid={ `${index}-horizontal-image` }
+            src={ recipe.image }
+            alt={ recipe.name }
+          />
+        </Link>
+        <span data-testid={ `${index}-horizontal-top-text` }>
+          { recipe.area.length > 0 ? `${recipe.area} - ` : ''}
+          { recipe.alcoholicOrNot.length > 0 ? `${recipe.alcoholicOrNot} - ` : ''}
+          { recipe.category }
+        </span>
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <p data-testid={ `${index}-horizontal-name` }>
+            { recipe.name }
+          </p>
+        </Link>
+        <ShareButton type={ recipe.type } id={ recipe.id } index={ index } />
+        <FavoreButton data={ recipe } aux={ aux } setAux={ setAux } index={ index } />
+      </div>
+    ))}
+  </div>
+);
 
-  useEffect(() => {
-    if (localStorage.favoriteRecipes) {
-      setLocal(JSON.parse(localStorage.favoriteRecipes));
-    }
-  }, [aux]);
-
-  useEffect(() => {
-    if (local) {
-      setRecipes(JSON.parse(localStorage.favoriteRecipes));
-    }
-  }, [local]);
-
-  return (
-    <div>
-      {recipes && recipes.map((recipe, index) => (
-        <div
-          className={ styles.card }
-          key={ index }
-        >
-          <Link to={ `/${recipe.type}s/${recipe.id}` }>
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              src={ recipe.image }
-              alt={ recipe.name }
-            />
-          </Link>
-          <span data-testid={ `${index}-horizontal-top-text` }>
-            { recipe.area.length > 0 ? `${recipe.area} - ` : ''}
-            { recipe.alcoholicOrNot.length > 0 ? `${recipe.alcoholicOrNot} - ` : ''}
-            { recipe.category }
-          </span>
-          <Link to={ `/${recipe.type}s/${recipe.id}` }>
-            <p data-testid={ `${index}-horizontal-name` }>
-              { recipe.name }
-            </p>
-          </Link>
-          <ShareButton type={ recipe.type } id={ recipe.id } index={ index } />
-          <FavoreButton data={ recipe } aux={ aux } setAux={ setAux } index={ index } />
-        </div>
-      ))}
-    </div>
-  );
+FavCard.propTypes = {
+  recipes: PropTypes.shape([]).isRequired,
+  aux: PropTypes.bool.isRequired,
+  setAux: PropTypes.func.isRequired,
 };
 
 export default FavCard;
--- src/pages/recipes/RecipesFav.jsx
+++ src/pages/recipes/RecipesFav.jsx
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from '../../components';
 import FavCard from '../../components/favorite/FavCard';
 
-const RecipesFav = () => (
-  <div>
-    <Header title="Receitas Favoritas" search={ false } />
-    <button type="button" data-testid="filter-by-all-btn">
-      All
-    </button>
-    <button type="button" data-testid="filter-by-food-btn">
-      food
-    </button>
+const RecipesFav = () => {
+  const [aux, setAux] = useState(false);
+  const [recipes, setRecipes] = useState(null);
+  const [filter, setFilter] = useState('all');
+  const [local, setLocal] = useState(null);
 
-    <button type="button" data-testid="filter-by-drink-btn">
-      drink
-    </button>
-    <FavCard />
-  </div>
-);
+  useEffect(() => {
+    if (localStorage.favoriteRecipes) {
+      setLocal(JSON.parse(localStorage.favoriteRecipes));
+    }
+  }, [aux]);
+
+  useEffect(() => {
+    if (local) {
+      if (filter === 'all') setRecipes(JSON.parse(localStorage.favoriteRecipes));
+      else {
+        setRecipes(JSON.parse(localStorage.favoriteRecipes)
+          .filter((recipe) => recipe.type === filter));
+      }
+    }
+  }, [local, filter]);
+
+  return (
+    <div>
+      <Header title="Receitas Favoritas" search={ false } />
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => { setFilter('all'); } }
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ () => { setFilter('comida'); } }
+      >
+        Comidas
+      </button>
+
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => { setFilter('bebida'); } }
+      >
+        Bebidas
+      </button>
+      { recipes && <FavCard recipes={ recipes } aux={ aux } setAux={ setAux } /> }
+    </div>
+  );
+};
 
 export default RecipesFav;
